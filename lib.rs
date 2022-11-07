51e008bd8a3df52b277563a704c7f4c8a54ffbd9
KowalewskiPawel <pawelkowalewsk@gmail.com>
2022-11-07 22:40:28 +0100

    contract added

--- lib.rs
+++ lib.rs
@@ -3,72 +3,88 @@
 use ink_lang as ink;
 
 #[ink::contract]
-mod token_test {
+mod mytoken {
+    use ink_storage::{traits::SpreadAllocate, Mapping};
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
     #[ink(storage)]
-    pub struct TokenTest {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+    #[derive(SpreadAllocate)]
+    pub struct Mytoken {
+        total_supply: u32,
+        balances: Mapping<AccountId, u32>,
     }
 
-    impl TokenTest {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
+    use ink_lang::utils::initialize_contract;
+    impl Mytoken {
+        /// Creates a token contract with the given initial supply belonging to the contract creator.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new_token(supply: u32) -> Self {
+            initialize_contract(|contract: &mut Self| {
+                let caller = Self::env().caller();
+                contract.balances.insert(&caller, &supply);
+                contract.total_supply = supply;
+            })
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
+        /// Total supply of the token.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn total_supply(&self) -> u32 {
+            self.total_supply
         }
 
-        /// Simply returns the current value of our `bool`.
+        /// Current balance of the chosen account.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn balance_of(&self, account: AccountId) -> u32 {
+            match self.balances.get(&account) {
+                Some(value) => value,
+                None => 0,
+            }
+        }
+
+        /// Transfers an amount of tokens to the chosen recipient.
+        #[ink(message)]
+        pub fn transfer(&mut self, recipient: AccountId, amount: u32) {
+            let sender = self.env().caller();
+            let sender_balance = self.balance_of(sender);
+            if sender_balance < amount {
+                return;
+            }
+            self.balances.insert(sender, &(sender_balance - amount));
+            let recipient_balance = self.balance_of(recipient);
+            self.balances.insert(recipient, &(recipient_balance + amount));
         }
     }
+}
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
+#[cfg(test)]
+mod tests {
+    use crate::mytoken::Mytoken;
+    use ink_env::{test, DefaultEnvironment};
+    use ink_lang as ink;
 
-        /// Imports `ink_lang` so we can use `#[ink::test]`.
-        use ink_lang as ink;
+    #[ink::test]
+    fn total_supply_works() {
+        let mytoken = Mytoken::new_token(1000);
+        assert_eq!(mytoken.total_supply(), 1000);
+    }
 
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let token_test = TokenTest::default();
-            assert_eq!(token_test.get(), false);
-        }
+    #[ink::test]
+    fn balance_of_works() {
+        let accounts = test::default_accounts::<DefaultEnvironment>();
+        test::set_caller::<DefaultEnvironment>(accounts.alice);
+        let mytoken = Mytoken::new_token(1000);
+        assert_eq!(mytoken.balance_of(accounts.alice), 1000);
+        assert_eq!(mytoken.balance_of(accounts.bob), 0);
+    }
 
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut token_test = TokenTest::new(false);
-            assert_eq!(token_test.get(), false);
-            token_test.flip();
-            assert_eq!(token_test.get(), true);
-        }
+    #[ink::test]
+    fn transfer_works() {
+        let accounts = test::default_accounts::<DefaultEnvironment>();
+        test::set_caller::<DefaultEnvironment>(accounts.alice);
+        let mut mytoken = Mytoken::new_token(1000);
+        assert_eq!(mytoken.balance_of(accounts.alice), 1000);
+        assert_eq!(mytoken.balance_of(accounts.bob), 0);
+        mytoken.transfer(accounts.bob, 100);
+        assert_eq!(mytoken.balance_of(accounts.alice), 900);
+        assert_eq!(mytoken.balance_of(accounts.bob), 100);
     }
 }
